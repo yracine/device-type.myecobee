4ddd540562c52f7351d8d653d83af6ea68c96a21
yracine <yracine66@gmail.com>
2014-06-22 07:02:29 -0400

    -Removed some comments at the beginning

--- ecobee.devicetype.groovy
+++ ecobee.devicetype.groovy
@@ -1,41 +1,4 @@
-/***
- *  My Ecobee Device
- *
- *  Author: Yves Racine
- *  linkedIn profile: ca.linkedin.com/pub/yves-racine-m-sc-a/0/406/4b/
- *  Date: 2014-03-31
- *  Code: https://github.com/yracine/device-type.myecobee
- *
- * INSTALLATION STEPS
- * ==================
- *
- * 1) Connect to the ecobee portal (www.ecobee.com) and create an application key with an application name (such as ecobeeTstat)
- *    and indicate the PIN method authentication (at the bottom of the window).
- *
- * 2) Create a new device type (https://graph.api.smartthings.com/ide/devices)
- *     Name: MyEcobee Device
- *     Author: Yves Racine
- *    
- * 3) Create a new device (https://graph.api.smartthings.com/device/list)
- *     Name: Your Choice
- *     Device Network Id: Your Choice
- *     Type: My Ecobee Device (should be the last option)
- *     Location: Choose the correct location
- *     Hub/Group: (optional) leave blank or set it up to your liking
- *
- * 4) Update device's preferences
- *     Click on the new device 
- *     Click the edit button next to Preferences
- *     Fill in your device 
- *        (a) <appKey> provided at the ecobee web portal in step 1
- *        (b) <serial number> of your ecobee thermostat
- *        (c) <trace> when needed, set to true to get more tracing
- *        (d) <holdType> set to nextTransition or indefinite (by default) 
- *        see https://www.ecobee.com/home/developer/api/documentation/v1/functions/SetHold.shtml for more details
- *        (e) <ecobeeType> set to 'registered' (by default) or 'managementSet'
- *
- * 5) To get an ecobee PIN (double authentication), create a small app with the following code and install it.
- *
+
 /***
  *  My Ecobee Device
  *
