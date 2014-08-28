5c96450eecd5076a8cd54104ec726b6fc74c0102
yracine <yracine66@gmail.com>
2014-08-28 10:31:46 -0400

    -Added the temperature tile in Farenheits
    -Added the temperature tile in Farenheits as comments (need to be commented out for Farenheits users)

--- ecobee.devicetype.groovy
+++ ecobee.devicetype.groovy
@@ -155,7 +155,7 @@ metadata {
 		valueTile("temperature", "device.temperature", width: 2, height: 2,
 			canChangeIcon: true) {
 			state("temperature", label: '${currentValue}°', unit: "C", 
-            	backgroundColors: [
+            		backgroundColors: [
 					[value: 0, color: "#153591"],
 					[value: 8, color: "#1e9cbb"],
 					[value: 14, color: "#90d2a7"],
@@ -165,6 +165,21 @@ metadata {
 					[value: 36, color: "#bc2323"]
 				])
 		}
+//		Temperature tile in Farenheits 
+//		valueTile("temperature", "device.temperature", width: 2, height: 2) {
+//			state("temperature", label:'${currentValue}°', unit:"F",
+//			backgroundColors:[
+//					[value: 31, color: "#153591"],
+//					[value: 44, color: "#1e9cbb"],
+//					[value: 59, color: "#90d2a7"],
+//					[value: 74, color: "#44b621"],
+//					[value: 84, color: "#f1d801"],
+//					[value: 95, color: "#d04e00"],
+//					[value: 96, color: "#bc2323"]
+//				])
+//		}
+
+
 		standardTile("mode", "device.thermostatMode", inactiveLabel: false,
 			decoration: "flat") {
 			state "heat", label: '${name}', action: "thermostat.off", 
