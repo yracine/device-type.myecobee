3868ec54c1b91653bfd1dd7b14cdc654c0ac0e80
Yves Racine <yracine66@gmail.com>
2015-01-10 18:26:06 -0500

    Minor code formatting correction

--- ecobee.devicetype.groovy
+++ ecobee.devicetype.groovy
@@ -1877,7 +1877,7 @@ void setClimate(thermostatId, climateName) {
 			}        
 			continue
 		}
-        	tstatParams =((settings.holdType != null) && (settings.holdType.trim() != "")) ?
+        tstatParams =((settings.holdType != null) && (settings.holdType.trim() != "")) ?
 			[holdClimateRef:"${climateRef}", holdType:"${settings.holdType.trim()}"
 			] :
 			[holdClimateRef:"${climateRef}"
