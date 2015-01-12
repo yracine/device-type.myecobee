570fa47aadee46a5c19a2e5378efe5a33a300d73
Yves Racine <yracine66@gmail.com>
2015-01-12 14:00:58 -0500

    Modified header

--- smartapps/My ecobee Init.groovy
+++ smartapps/My ecobee Init.groovy
@@ -9,7 +9,8 @@
  *      JLH - 02-15-2014 - Fuller use of ecobee API
  *		Y.Racine Nov 2014 - Simplified the Service Manager as much as possible to reduce tight coupling with 
  *							its child device types (device handlers) for better performance and reliability.
- */
+ *                       linkedIn profile: ca.linkedin.com/pub/yves-racine-m-sc-a/0/406/4b/ 
+  */
 definition(
     name: "My ecobee Init",
     namespace: "yracine",
