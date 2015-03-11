2188798791c0419f59649fa15872c614299a4f54
Yves Racine <yracine66@gmail.com>
2015-03-11 14:40:20 -0400

    Changed description

--- smartapps/MonitorAndSetEcobeeTemp.groovy
+++ smartapps/MonitorAndSetEcobeeTemp.groovy
@@ -39,7 +39,7 @@ preferences {
 def thresholdSettings() {
 	dynamicPage(name: "thresholdSettings", install: false, uninstall: true, nextPage: "sensorSettings") {
 		section("About") {	
-			paragraph "MonitorAndSetEcobeeTemp,the smartapp that adjusts your ecobee's setpoints based on in/out-door sensors"
+			paragraph "MonitorAndSetEcobeeTemp,the smartapp that adjusts your ecobee's setpoints based on indoor/outdoor sensors"
 			paragraph "Version 1.9\n\n" +
 			"If you like this app, please support the developer via PayPal:\n\nyracine@yahoo.com\n\n" +
 			"Copyright©2014 Yves Racine"
@@ -861,4 +861,3 @@ def cToF(temp) {
 def fToC(temp) {
 	return (temp - 32) / 1.8
 }
-
