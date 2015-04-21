5aeb539d0b291fe02f8a2111d6a0979ae8363f45
Yves Racine <yracine66@gmail.com>
2015-04-21 19:27:09 -0400

    Corrected an issue related to presence

--- ecobee.devicetype.groovy
+++ ecobee.devicetype.groovy
@@ -766,8 +766,7 @@ void poll() {
 
 	ecobeeType = determine_ecobee_type_or_location(ecobeeType)
 	def progDisplayName = getCurrentProgName()
-	def currentClimateTemplate = device.currentValue("setClimate")
-	currentClimateTemplate= (currentClimateTemplate)? currentClimateTemplate: progDisplayName  // if not climate template set, then use current program
+	def currentClimateTemplate= (data.thermostatList[0].program.currentClimateRef)? data.thermostatList[0].program.currentClimateRef: progDisplayName  // if not climate template set, then use current program
 	if (settings.trace) {
 		log.debug "poll>thermostatId = ${thermostatId},Current Climate Ref=${data.thermostatList[0].program.currentClimateRef},currentClimateTemplate=${currentClimateTemplate}"
 		sendEvent name: "verboseTrace", value:
@@ -2679,8 +2678,8 @@ void generateRemoteSensorEvents(thermostatId,postData='false') {
 		getThermostatInfo(thermostatId)    
 	}
 	thermostatId = determine_tstat_id(thermostatId)
-    
-	/* Reset all remote sensor data values */
+
+/* Reset all remote sensor data values */
 	def remoteData = []
 	def remoteTempData = ""
 	def remoteHumData = ""
