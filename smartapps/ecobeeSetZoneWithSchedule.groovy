528a81880f415001c1762aa859a7e16c16fec27f
Yves Racine <yracine66@gmail.com>
2015-07-16 15:30:03 -0400

    V1.1. Removed subscription for programScheduleName

--- smartapps/ecobeeSetZoneWithSchedule.groovy
+++ smartapps/ecobeeSetZoneWithSchedule.groovy
@@ -43,7 +43,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ecobeeSetZoneWithSchedule, the smartapp that enables Heating/Cooling Zoned Solutions based on your ecobee schedule(s)- coupled with z-wave vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 1.0\n\n" +
+			paragraph "Version 1.1\n\n" +
 				"If you like this app, please support the developer via PayPal:\n\nyracine@yahoo.com\n\n" +
 				"Copyright©2015 Yves Racine"
 			href url: "http://github.com/yracine", style: "embedded", required: false, title: "More information...",
@@ -401,7 +401,6 @@ def initialize() {
 	runEvery5Minutes(setZoneSettings)
 
 	subscribe(app, appTouch)
-	subscribe(thermostat, "programScheduleName", setZoneSettings)    
 }
 
 def appTouch(evt) {
