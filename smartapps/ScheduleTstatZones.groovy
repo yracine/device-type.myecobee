7e162cecd3ce0831d851b0d7061045021cb8f452
Yves Racine <yracine66@gmail.com>
2015-07-29 12:12:20 -0400

    Corrected version number

--- smartapps/ScheduleTstatZones.groovy
+++ smartapps/ScheduleTstatZones.groovy
@@ -44,7 +44,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ScheduleTstatZones, the smartapp that enables Heating/Cooling zoned settings at selected thermostat(s) coupled with z-wave vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 1.9.1\n\n" +
+			paragraph "Version 1.9.2\n\n" +
 				"If you like this app, please support the developer via PayPal:\n\nyracine@yahoo.com\n\n" +
 				"Copyright©2015 Yves Racine"
 			href url: "http://github.com/yracine", style: "embedded", required: false, title: "More information...",
