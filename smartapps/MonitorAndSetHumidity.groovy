2bdcef19b9834e23a9942923d16c625665541d01
Yves Racine <yracine66@gmail.com>
2015-09-07 06:57:05 -0400

    v1.9.7 changed header

--- smartapps/MonitorAndSetHumidity.groovy
+++ smartapps/MonitorAndSetHumidity.groovy
@@ -41,11 +41,12 @@ def humiditySettings() {
 	dynamicPage(name: "humiditySettings", install: false, uninstall: true, nextPage: "sensorSettings") {
 		section("About") {
 			paragraph "MonitorAndSetEcobeeHumdity, the smartapp that can control your house's humidity via your connected humidifier/dehumidifier/HRV/ERV"
-			paragraph "Version 1.9.6\n\n" +
-				"If you like this app, please support the developer via PayPal:\n\nyracine@yahoo.com\n\n" +
-				"Copyright©2014 Yves Racine"
-			href url: "http://github.com/yracine/device-type.myecobee", style: "embedded", required: false, title: "More information...",
-			description: "http://github.com/yracine/device-type.myecobee/blob/master/README.md"
+			paragraph "Version 1.9.7"
+			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Next Page link below " 
+			href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest" 
+			paragraph "Copyright©2014 Yves Racine"
+			href url:"http://github.com/yracine/device-type.myecobee", style:"embedded", required:false, title:"More information..."  
+ 				description: "http://github.com/yracine/device-type.myecobee/blob/master/README.md"
 		}
 		section("Monitor & set the ecobee thermostat's dehumidifer/humidifier/HRV/ERV") {
 			input "ecobee", "capability.thermostat", title: "Ecobee?"
