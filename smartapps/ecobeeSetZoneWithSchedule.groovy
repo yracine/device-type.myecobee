61c8ee8b62953b9c2474cdce5e6c1213604a5c3b
Yves Racine <yracine66@gmail.com>
2015-09-09 15:01:55 -0400

    Minor change to header

--- smartapps/ecobeeSetZoneWithSchedule.groovy
+++ smartapps/ecobeeSetZoneWithSchedule.groovy
@@ -44,11 +44,12 @@ def generalSetupPage() {
 		section("About") {
 			paragraph "ecobeeSetZoneWithSchedule, the smartapp that enables Heating/Cooling Zoned Solutions based on your ecobee schedule(s)- coupled with z-wave vents (optional) for better temp settings control throughout your home"
 			paragraph "Version 2.4.1" 
-			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Next Page link below " 
-			href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest" 
+			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
+				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
+					title:"Paypal donation..."
 			paragraph "Copyright©2015 Yves Racine"
-			href url:"http://github.com/yracine/device-type.myecobee", style:"embedded", required:false, title:"More information..."  
-				description: "http://github.com/yracine/device-type.myecobee/blob/master/README.md"
+				href url:"http://github.com/yracine/device-type.myecobee", style:"embedded", required:false, title:"More information..."  
+					description: "http://github.com/yracine/device-type.myecobee/blob/master/README.md"
  		}
 		section("Main ecobee thermostat at home") {
 			input (name:"thermostat", type: "capability.thermostat", title: "Which main ecobee thermostat?")
