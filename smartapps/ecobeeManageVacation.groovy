43b1451d55012c8ebf1525f0beb3662ac027740c
Yves Racine <yracine66@gmail.com>
2015-09-09 14:56:40 -0400

    Minor change to header

--- smartapps/ecobeeManageVacation.groovy
+++ smartapps/ecobeeManageVacation.groovy
@@ -31,11 +31,12 @@ preferences {
 	section("About") {
 		paragraph "ecobeeManageVacation, the smartapp that manages your ecobee vacation settings ['creation', 'update', 'delete']"
 		paragraph "Version 1.9.1" 
-		paragraph "If you like this smartapp, please support the developer via PayPal and click on the Next Page link below " 
-		href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest" 
+		paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
+			href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
+				title:"Paypal donation..."
 		paragraph "Copyright©2014 Yves Racine"
-		href url:"http://github.com/yracine/device-type.myecobee", style:"embedded", required:false, title:"More information..."  
-			description: "http://github.com/yracine/device-type.myecobee/blob/master/README.md"
+			href url:"http://github.com/yracine/device-type.myecobee", style:"embedded", required:false, title:"More information..."  
+				description: "http://github.com/yracine/device-type.myecobee/blob/master/README.md"
 	}
 
 	section("For the Ecobee thermostat(s)") {
