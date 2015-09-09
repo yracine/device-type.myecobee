2a8d4ed4e93fddf93401f7a9aaf4b2c93e8e1101
Yves Racine <yracine66@gmail.com>
2015-09-09 14:58:01 -0400

    Minor change to header

--- smartapps/ecobeeResumeProg.groovy
+++ smartapps/ecobeeResumeProg.groovy
@@ -32,11 +32,12 @@ preferences {
 		section("About") {
 			paragraph "ecobeeResumeProg, the smartapp that resumes your ecobee's scheduled program when a presence is back home,or when motion is detected or when a ST hello mode is changed"
 			paragraph "Version 2.1.1" 
-			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Next Page link below " 
-			href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest" 
+			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
+				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
+					title:"Paypal donation..."
 			paragraph "Copyright©2014 Yves Racine"
-			href url:"http://github.com/yracine/device-type.myecobee", style:"embedded", required:false, title:"More information..."  
-				description: "http://github.com/yracine/device-type.myecobee/blob/master/README.md"
+				href url:"http://github.com/yracine/device-type.myecobee", style:"embedded", required:false, title:"More information..."  
+					description: "http://github.com/yracine/device-type.myecobee/blob/master/README.md"
 		}
 	}        
 	page(name: "selectThermostats", title: "Thermostats", install: false , uninstall: false, nextPage: "selectModes") {
