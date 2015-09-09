141b1d74b4c3bfd1aa3479a67d15dab463e4ba65
Yves Racine <yracine66@gmail.com>
2015-09-09 15:00:32 -0400

    Minor change to header

--- smartapps/ecobeeSetFanMinOnTime.groovy
+++ smartapps/ecobeeSetFanMinOnTime.groovy
@@ -30,11 +30,12 @@ preferences {
 		section("About") {
 			paragraph "ecobeeSetFanMinOnTime, the smartapp that sets your ecobee's fan to circulate for a minimum time (in minutes) per hour." 
 			paragraph "Version 1.0" 
-			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Next Page link below " 
-			href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest" 
+			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
+				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
+					title:"Paypal donation..."
 			paragraph "Copyright©2015 Yves Racine"
-			href url:"http://github.com/yracine/device-type.myecobee", style:"embedded", required:false, title:"More information..."  
-				description: "http://github.com/yracine/device-type.myecobee/blob/master/README.md"
+				href url:"http://github.com/yracine/device-type.myecobee", style:"embedded", required:false, title:"More information..."  
+					description: "http://github.com/yracine/device-type.myecobee/blob/master/README.md"
 		}
 		section("Change the following ecobee thermostat(s)...") {
 			input "thermostats", "device.myEcobeeDevice", title: "Which thermostat(s)", multiple: true
