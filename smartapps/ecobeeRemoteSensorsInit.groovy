d422e6e344b03263ca911053a336dffc51ba2ff7
Yves Racine <yracine66@gmail.com>
2015-09-09 14:47:22 -0400

    Minor Change to header

--- smartapps/ecobeeRemoteSensorsInit.groovy
+++ smartapps/ecobeeRemoteSensorsInit.groovy
@@ -30,10 +30,11 @@ preferences {
 		section("About") {
 			paragraph "ecobeeRemoteSensorsInit, the smartapp that creates individual ST sensors for your ecobee3's remote Sensors and polls them on a regular basis"
 			paragraph "Version 1.2" 
-			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Next Page link below " 
-			href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest" 
+			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
+				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
+					title:"Paypal donation..."
 			paragraph "Copyright©2014 Yves Racine"
-			href url:"http://github.com/yracine/device-type.myecobee", style:"embedded", required:false, title:"More information..." 
+				href url:"http://github.com/yracine/device-type.myecobee", style:"embedded", required:false, title:"More information..." 
 		}
 		section("Select the ecobee thermostat") {
 			input "ecobee", "capability.thermostat", title: "Which ecobee thermostat?"
