48fb5720e806fe4244a4c254d17cb6304e965ada
Yves Racine <yracine66@gmail.com>
2015-09-09 15:04:23 -0400

    Minor change to header

--- smartapps/groveStreams.groovy
+++ smartapps/groveStreams.groovy
@@ -30,11 +30,12 @@ preferences {
 	section("About") {
 		paragraph "groveStreams, the smartapp that sends your device states to groveStreams for data correlation"
 		paragraph "Version 1.9.1" 
-		paragraph "If you like this smartapp, please support the developer via PayPal and click on the Next Page link below " 
-		href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest" 
+			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
+				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
+					title:"Paypal donation..."
 		paragraph "Copyright©2014 Yves Racine"
-		href url:"http://github.com/yracine/device-type.myecobee", style:"embedded", required:false, title:"More information..."  
-			description: "http://github.com/yracine"
+			href url:"http://github.com/yracine/device-type.myecobee", style:"embedded", required:false, title:"More information..."  
+				description: "http://github.com/yracine"
 	}
 	section("Log devices...") {
 		input "temperatures", "capability.temperatureMeasurement", title: "Temperatures", required: false, multiple: true
