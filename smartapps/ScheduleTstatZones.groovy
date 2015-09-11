337b1bae31173ecc27fb55ed08d6d7a6040dab86
Yves Racine <yracine66@gmail.com>
2015-09-10 20:30:24 -0400

    v2.3.1 minor notification change

--- smartapps/ScheduleTstatZones.groovy
+++ smartapps/ScheduleTstatZones.groovy
@@ -44,7 +44,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ScheduleTstatZones, the smartapp that enables Heating/Cooling zoned settings at selected thermostat(s) coupled with z-wave vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 2.3" 
+			paragraph "Version 2.3.1" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -1346,7 +1346,7 @@ private def adjust_vent_settings_in_zone(indiceSchedule) {
 			float max_temp_diff = input_max_temp_diff.toFloat().round(1)
 			if (avg_temp_diff.abs() > max_temp_diff) {
 				if (detailedNotif == 'true') {
-					send("ScheduleTstatZones>schedule ${scheduleName}, in zone ${zoneName}, avg_temp_diff=${avg_temp_diff.abs()} > ${max_temp_diff},ajusting fan mode")
+					send("ScheduleTstatZones>schedule ${scheduleName},in zone ${zoneName},avg_temp_diff=${avg_temp_diff.abs()} > ${max_temp_diff}:adjusting fan as temp differential too big")
 				}
 				set_fan_mode(indiceSchedule)                
 			}                
