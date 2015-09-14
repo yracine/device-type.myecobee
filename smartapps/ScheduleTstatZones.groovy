c90eb2806f1bd9ed82d8fb0d1a4bf50819d20140
Yves Racine <yracine66@gmail.com>
2015-09-14 18:55:02 -0400

    v2.3.9 Some changes to setZoneSettings()

--- smartapps/ScheduleTstatZones.groovy
+++ smartapps/ScheduleTstatZones.groovy
@@ -44,7 +44,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ScheduleTstatZones, the smartapp that enables Heating/Cooling zoned settings at selected thermostat(s) coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 2.3.8" 
+			paragraph "Version 2.3.9" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -522,11 +522,11 @@ def setZoneSettings() {
 	thermostat.poll()
 
 
-	if ((outTempSensor) && (outTempSensor.hasCapability("Refresh"))) {
+	if ((outTempSensor) && ((outTempSensor.hasCapability("Refresh")) || (outTempSensor.hasCapability("Polling")))) {
 
 		// do a refresh to get latest temp value
 		try {        
-			outTempSensor.refresh()
+			outTempSensor.poll()
 		} catch (e) {
 			log.debug("setZoneSettings>not able to do a refresh() on $outTempSensor")
 		}                    
