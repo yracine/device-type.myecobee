687b96fbe0c181ec3c9425534bd5797b2e0498a9
Yves Racine <yracine66@gmail.com>
2015-09-20 02:48:25 -0400

    v2.4.3 set Level at a minimum of 25 under certain conditions

--- smartapps/ScheduleTstatZones.groovy
+++ smartapps/ScheduleTstatZones.groovy
@@ -44,7 +44,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ScheduleTstatZones, the smartapp that enables Heating/Cooling zoned settings at selected thermostat(s) coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 2.4.2" 
+			paragraph "Version 2.4.3" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -1407,7 +1407,7 @@ private def adjust_vent_settings_in_zone(indiceSchedule) {
 
 	if (closedAllVentsInZone) {
     
-		switchLevel=(nbVents>2)? 10:15        
+		switchLevel=(nbVents>2)? 10:25        
 		ventSwitchesOnSet=control_vent_switches_in_zone(indiceSchedule, switchLevel)		    
 		log.debug "adjust_vent_settings_in_zone>schedule ${scheduleName}, set all ventSwitches at ${switchLevel}% to avoid closing all of them"
 		if (detailedNotif == 'true') {
