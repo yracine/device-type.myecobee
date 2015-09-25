8516f2d7bf786634d745bda555067b88aff7af4e
Yves Racine <yracine66@gmail.com>
2015-09-25 13:21:43 -0400

    v3.1.1 Added call to setZoneSettings() in setClimateHandler

--- smartapps/ecobeeSetZoneWithSchedule.groovy
+++ smartapps/ecobeeSetZoneWithSchedule.groovy
@@ -43,7 +43,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ecobeeSetZoneWithSchedule, the smartapp that enables Heating/Cooling Zoned Solutions based on your ecobee schedule(s)- coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 3.1" 
+			paragraph "Version 3.1.1" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -421,6 +421,9 @@ def motionEvtHandler(evt) {
 
 def setClimateHandler(evt) {
 	log.debug "SetClimate, $evt.name: $evt.value"
+    
+	setZoneSettings()  
+    
 }
 
 def initialize() {
@@ -429,7 +432,7 @@ def initialize() {
 		subscribe(powerSwitch, "switch.off", offHandler, [filterEvents: false])
 		subscribe(powerSwitch, "switch.on", onHandler, [filterEvents: false])
 	}
-	subscribe(thermostat, "setClimate", setClimateHandler)
+	subscribe(thermostat, "climateName", setClimateHandler)
 	// Initialize state variables
     
 	state.lastScheduleLastName=""
