02a9319fec83c487235d73bd71ed38bc2a3fb6d1
Yves Racine <yracine66@gmail.com>
2015-09-27 20:57:48 -0400

    v2.7.2 Minor changes to label in Schedule page

--- smartapps/ScheduleTstatZones.groovy
+++ smartapps/ScheduleTstatZones.groovy
@@ -44,7 +44,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ScheduleTstatZones, the smartapp that enables Heating/Cooling zoned settings at selected thermostat(s) coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 2.7.1" 
+			paragraph "Version 2.7.2" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -357,15 +357,15 @@ def schedulesSetup(params) {
 			input (name:"coolModeThreshold${indiceSchedule}", type:"decimal", title: "Cool mode threshold", 
 				required: false, defaultValue:settings."coolModeThreshold${indiceSchedule}", description: "Optional")			               
 		}			
-		section("Schedule ${indiceSchedule}-Select the program at ecobee thermostat to be applied [optional,for ecobee]") {
+		section("Schedule ${indiceSchedule}-Select the program/climate at ecobee thermostat to be applied [optional,for ecobee]") {
 			input (name:"givenClimate${indiceSchedule}", type:"enum", title: "Which ecobee program? ", options: ecobeePrograms, 
 				required: false, defaultValue:settings."givenClimate${indiceSchedule}", description: "Optional")
 		}
-		section("Schedule ${indiceSchedule}-Desired Cool Temp in the selected zone(s) [optional,when no program/climate settings]") {
+		section("Schedule ${indiceSchedule}-Desired Cool Temp in the selected zone(s) [optional,when no program/climate available]") {
 			input (name:"desiredCoolTemp${indiceSchedule}", type:"decimal", title: "Cool Temp, default = 75°F/23°C", 
 				required: false,defaultValue:settings."desiredCoolTemp${indiceSchedule}", description: "Optional")			                
 		}
-		section("Schedule ${indiceSchedule}-Desired Heat Temp in the selected zone(s) [optional,when no program/climate settings]") {
+		section("Schedule ${indiceSchedule}-Desired Heat Temp in the selected zone(s) [optional,when no program/climate available]") {
 			input (name:"desiredHeatTemp${indiceSchedule}", type:"decimal", title: "Heat Temp, default=72°F/21°C", 
 				required: false, defaultValue:settings."desiredHeatTemp${indiceSchedule}", description: "Optional")			                
 		}
