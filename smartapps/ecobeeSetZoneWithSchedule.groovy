faa61872fa7bd1accad63a7dea037aea9ffb1a57
Yves Racine <yracine66@gmail.com>
2015-09-30 08:04:32 -0400

    v3.6.2 adjusted min open level in adjust_vent_settings_in_zone()

--- smartapps/ecobeeSetZoneWithSchedule.groovy
+++ smartapps/ecobeeSetZoneWithSchedule.groovy
@@ -43,7 +43,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ecobeeSetZoneWithSchedule, the smartapp that enables Heating/Cooling Zoned Solutions based on your ecobee schedule(s)- coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 3.6.1" 
+			paragraph "Version 3.6.2" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -1423,6 +1423,8 @@ private def adjust_thermostat_setpoint_in_zone(indiceSchedule) {
 
 
 private def adjust_vent_settings_in_zone(indiceSchedule) {
+	def MIN_OPEN_LEVEL_SMALL=25
+	def MIN_OPEN_LEVEL_BIG=35
 	float desiredTemp, avg_indoor_temp, avg_temp_diff
 
 	def key = "scheduleName$indiceSchedule"
@@ -1515,7 +1517,7 @@ private def adjust_vent_settings_in_zone(indiceSchedule) {
 
 	if (closedAllVentsInZone) {
 		    	
-		switchLevel=(nbVents>2)? 10:25        
+		switchLevel=(nbVents>2)? MIN_OPEN_LEVEL_SMALL:MIN_OPEN_LEVEL_BIG        
 		ventSwitchesOnSet=control_vent_switches_in_zone(indiceSchedule, switchLevel)		    
 		log.debug "adjust_vent_settings_in_zone>schedule ${scheduleName}, set all ventSwitches at ${switchLevel}% to avoid closing all of them"
 		if (detailedNotif == 'true') {
