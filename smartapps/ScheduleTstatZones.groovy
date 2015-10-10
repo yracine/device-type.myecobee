2696e4216b39617a40bcf08f535c0df57635e207
Yves Racine <yracine66@gmail.com>
2015-10-10 14:11:34 -0400

    v4.0.2 minor change in is_temperature_in_vent_too_hot()

--- smartapps/ScheduleTstatZones.groovy
+++ smartapps/ScheduleTstatZones.groovy
@@ -44,7 +44,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ScheduleTstatZones, the smartapp that enables Heating/Cooling zoned settings at selected thermostat(s) coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 3.0.1" 
+			paragraph "Version 3.0.2" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -1506,7 +1506,7 @@ private boolean is_temperature_in_vent_too_hot(ventSwitch) {
 		} /* end for rooms */
         
 		send("ScheduleTstatZones> ** IMMEDIATE ATTENTION: inside temperature of vent ${ventSwitch} is too high (${tempSwitch}°), opening all vents and turning off the HVAC to avoid any damage **")
-		powerSwitch.off()                        
+		powerSwitch?.off()                        
 		return true
 	}          
 	return false
