774a287b1a3a33a699669f97d1e817befac40845
Yves Racine <yracine66@gmail.com>
2015-10-10 16:46:28 -0400

    v4.0.4 Added 'auto' temp check in turn_off_all_other_vents()

--- smartapps/ecobeeSetZoneWithSchedule.groovy
+++ smartapps/ecobeeSetZoneWithSchedule.groovy
@@ -43,7 +43,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ecobeeSetZoneWithSchedule, the smartapp that enables Heating/Cooling Zoned Solutions based on your ecobee schedule(s)- coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 4.0.3" 
+			paragraph "Version 4.0.4" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -1565,8 +1565,9 @@ private def turn_off_all_other_vents(ventSwitchesOnSet) {
 				totalVents++
 				log.debug "turn_off_all_other_vents>found=${ventSwitch}, currentHVACMode=${currentHVACMode}"
 				// Prior to any processing, check temperature in each vent to avoid any HVAC damage
-				if (currentHVACMode=='heat') {
+				if ((currentHVACMode=='heat') || (currentHVACMode == 'auto')) {
 					if (is_temperature_in_vent_too_hot(ventSwitch)) {
+						log.debug("turn_off_all_other_vents>temperature too hot in ${ventSwitch}, exiting...")
 						return                        
 					}                            
 				}                    
