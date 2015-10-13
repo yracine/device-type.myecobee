9e23b9d759829d7fc8b0cf644d7ffe5e2195aa1e
Yves Racine <yracine66@gmail.com>
2015-10-13 08:44:36 -0400

    v2.0.5 minor change to header

--- smartapps/My ecobee Init.groovy
+++ smartapps/My ecobee Init.groovy
@@ -43,7 +43,7 @@ def about() {
  	dynamicPage(name: "about", install: false, uninstall: true) {
  		section("About") {	
 			paragraph "My Ecobee Init, the smartapp that connects your Ecobee thermostat to SmartThings via cloud-to-cloud integration"
-			paragraph "Version 2.0.4\n\n" +
+			paragraph "Version 2.0.5\n\n" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
