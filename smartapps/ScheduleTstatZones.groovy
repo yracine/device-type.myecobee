ff72362eb64f2fe1d194253ea41689ca187aa08d
Yves Racine <yracine66@gmail.com>
2015-10-18 14:25:43 -0400

    v3.4 Added subscription to any ST Hello Mode change

--- smartapps/ScheduleTstatZones.groovy
+++ smartapps/ScheduleTstatZones.groovy
@@ -44,7 +44,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ScheduleTstatZones, the smartapp that enables Heating/Cooling zoned settings at selected thermostat(s) coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 3.3.2" 
+			paragraph "Version 3.4" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -449,6 +449,11 @@ def coolingSetpointHandler(evt) {
 	log.debug "cooling Setpoint now: $evt.value"
 }
 
+def changeModeHandler(evt) {
+	log.debug "Changed mode, $evt.name: $evt.value"
+	setZoneSettings()  
+}
+
 def motionEvtHandler(evt) {
 	if (evt.value == "active") {
 		log.debug "Motion at home..."
@@ -467,6 +472,7 @@ def initialize() {
 	}
 	subscribe(thermostat, "heatingSetpoint", heatingSetpointHandler)    
 	subscribe(thermostat, "coolingSetpoint", coolingSetpointHandler)    
+	subscribe(location, changeMode)
 
 	// Initialize state variables
 	state.lastScheduleLastName=""
