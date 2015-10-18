339a9d8848ea6517db5c6671dee489fff97bb7bd
Yves Racine <yracine66@gmail.com>
2015-10-18 14:22:42 -0400

    v4.3 Added subscription to any ST hello mode change

--- smartapps/ecobeeSetZoneWithSchedule.groovy
+++ smartapps/ecobeeSetZoneWithSchedule.groovy
@@ -43,7 +43,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ecobeeSetZoneWithSchedule, the smartapp that enables Heating/Cooling Zoned Solutions based on your ecobee schedule(s)- coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 4.2.1" 
+			paragraph "Version 4.3" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -449,6 +449,8 @@ def initialize() {
 	}
 	subscribe(thermostat, "climateName", setClimateHandler)
 	subscribe(thermostat, "thermostatMode", changeModeHandler)
+	subscribe(location, changeModeHandler)
+
 	// Initialize state variables
     
 	state.lastScheduleLastName=""
