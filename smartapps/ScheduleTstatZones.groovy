fc1a443fad285a882eb781a7bca399b9d8fc7fa0
Yves Racine <yracine66@gmail.com>
2015-10-19 13:29:28 -0400

    v3.4.2 Minor correction to trace.

--- smartapps/ScheduleTstatZones.groovy
+++ smartapps/ScheduleTstatZones.groovy
@@ -44,7 +44,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ScheduleTstatZones, the smartapp that enables Heating/Cooling zoned settings at selected thermostat(s) coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 3.4.1" 
+			paragraph "Version 3.4.2" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -547,6 +547,7 @@ def setZoneSettings() {
 	def adjustmentTempFlag = (setAdjustmentTempFlag)?: 'false'
 	def adjustmentFanFlag = (setAdjustmentFanFlag)?: 'false'
     
+	String nowInLocalTime = new Date().format("yyyy-MM-dd HH:mm", location.timeZone)
 	for (int i = 1;((i <= settings.schedulesCount) && (i <= 12)); i++) {
         
 		def key = "selectedMode$i"
@@ -586,7 +587,6 @@ def setZoneSettings() {
 		}        
 		String startInLocalTime = startTimeToday.format("yyyy-MM-dd HH:mm", location.timeZone)
 		String endInLocalTime = endTimeToday.format("yyyy-MM-dd HH:mm", location.timeZone)
-		String nowInLocalTime = new Date().format("yyyy-MM-dd HH:mm", location.timeZone)
 
 		log.debug "setZoneSettings>found schedule ${scheduleName},original startTime=$startTime,original endTime=$endTime,nowInLocalTime= ${nowInLocalTime},startInLocalTime=${startInLocalTime},endInLocalTime=${endInLocalTime}," +
         		"currTime=${currTime},begintime=${startTimeToday.time},endTime=${endTimeToday.time},lastScheduleName=$state.lastScheduleName, lastStartTime=$state.lastStartTime"
