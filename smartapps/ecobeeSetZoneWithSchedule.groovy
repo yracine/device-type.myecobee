417b5b1c0d3cbb7e8e96511fe4c9a77cd6f12c6b
Yves Racine <yracine66@gmail.com>
2015-10-24 08:17:17 -0400

    Minor change to debug in rescheduleIfNeeded()

--- smartapps/ecobeeSetZoneWithSchedule.groovy
+++ smartapps/ecobeeSetZoneWithSchedule.groovy
@@ -497,7 +497,7 @@ def initialize() {
 
 
 def rescheduleIfNeeded(evt) {
-	log.debug("rescheduleIfNeeded>$evt.name=$evt.value")
+	if (evt) log.debug("rescheduleIfNeeded>$evt.name=$evt.value")
 	Integer delay = 5 // By default, schedule SetZoneSettings() every 5 min.
 	BigDecimal currentTime = now()    
 	BigDecimal lastPollTime = (currentTime - (state?.poll["last"]?:0))  
