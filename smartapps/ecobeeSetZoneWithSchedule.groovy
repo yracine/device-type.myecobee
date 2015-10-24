caf4e91044dcc75d7de02d43c6445a05b7ea34ba
Yves Racine <yracine66@gmail.com>
2015-10-24 08:06:43 -0400

    v4.3.4 changed rescheduleIfNeeded() signature

--- smartapps/ecobeeSetZoneWithSchedule.groovy
+++ smartapps/ecobeeSetZoneWithSchedule.groovy
@@ -43,7 +43,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ecobeeSetZoneWithSchedule, the smartapp that enables Heating/Cooling Zoned Solutions based on your ecobee schedule(s)- coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 4.3.3" 
+			paragraph "Version 4.3.4" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -496,7 +496,8 @@ def initialize() {
 
 
 
-def rescheduleIfNeeded() {
+def rescheduleIfNeeded(evt) {
+	log.debug("rescheduleIfNeeded>$evt.name=$evt.value")
 	Integer delay = 5 // By default, schedule SetZoneSettings() every 5 min.
 	BigDecimal currentTime = now()    
 	BigDecimal lastPollTime = (currentTime - (state?.poll["last"]?:0))  
