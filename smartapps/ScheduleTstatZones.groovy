622174c2955b29e2b19185e3f48fcb81b9a4f19e
Yves Racine <yracine66@gmail.com>
2015-11-06 21:45:02 -0500

    v3.6.2 Changed order of logic in adjust_thermostat_setpoint_in_zone()

--- smartapps/ScheduleTstatZones.groovy
+++ smartapps/ScheduleTstatZones.groovy
@@ -44,7 +44,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ScheduleTstatZones, the smartapp that enables Heating/Cooling zoned settings at selected thermostat(s) coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 3.6.1" 
+			paragraph "Version 3.6.2" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -1282,6 +1282,11 @@ private def adjust_thermostat_setpoint_in_zone(indiceSchedule) {
 	if (detailedNotif == 'true') {
 		send("ScheduleTstatZones>schedule ${scheduleName}:avg temp= ${avg_indoor_temp},main Tstat's currentTemp= ${currentTemp},temp adjustment=${temp_diff.abs()}")
 	}
+	key = "givenMaxTempDiff$indiceSchedule"
+	def givenMaxTempDiff = settings[key]
+	def input_max_temp_diff = givenMaxTempDiff ?: (scale=='C')? 2: 5 // 2°C/5°F temp differential is applied by default
+
+	float max_temp_diff = input_max_temp_diff.toFloat().round(1)
 
 	def adjustmentFanFlag = (setAdjustmentFanFlag)?: 'false'
 	if (adjustmentFanFlag=='true') {
@@ -1304,12 +1309,6 @@ private def adjust_thermostat_setpoint_in_zone(indiceSchedule) {
 		}
 		return
 	}                
-
-	key = "givenMaxTempDiff$indiceSchedule"
-	def givenMaxTempDiff = settings[key]
-	def input_max_temp_diff = givenMaxTempDiff ?: (scale=='C')? 2: 5 // 2°C/5°F temp differential is applied by default
-
-	float max_temp_diff = input_max_temp_diff.toFloat().round(1)
 
 	key = "givenClimate$indiceSchedule"
 	def climateName = settings[key]
