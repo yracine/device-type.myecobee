df01b5a25eee4a41f462d8986644fb9bd40b6b01
Yves Racine <yracine66@gmail.com>
2015-11-11 11:31:02 -0500

    V4.5.5 Minor correction to debug tracing

--- smartapps/ecobeeSetZoneWithSchedule.groovy
+++ smartapps/ecobeeSetZoneWithSchedule.groovy
@@ -43,7 +43,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ecobeeSetZoneWithSchedule, the smartapp that enables Heating/Cooling Zoned Solutions based on your ecobee schedule(s)- coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 4.5.4" 
+			paragraph "Version 4.5.5" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -1449,14 +1449,14 @@ private def adjust_thermostat_setpoint_in_zone(indiceSchedule) {
 		}
 	}
 	if (setRoomThermostatsOnly=='true') {
-		log.debug("adjust_thermostat_setpoint_in_zone>schedule ${scheduleName}: schedule ${scheduleName},all room Tstats set and setRoomThermostatsOnlyFlag= true,exiting")
+		log.debug("adjust_thermostat_setpoint_in_zone>schedule ${scheduleName},all room Tstats set and setRoomThermostatsOnlyFlag= true,exiting")
 		return				    
 	}    
     
     
 	if (state?.programHoldSet != "")  {
     
-		log.debug("adjust_thermostat_setpoint_in_zone>schedule ${scheduleName}: schedule ${scheduleName}, ${state?.programHoldSet} hold set ,exiting")
+		log.debug("adjust_thermostat_setpoint_in_zone>schedule ${scheduleName}, ${state?.programHoldSet} hold set ,exiting")
 		return				    
     
 	}    
