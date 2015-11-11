cf826a5af8f67f41b927b285a2e39f01f9927c9a
Yves Racine <yracine66@gmail.com>
2015-11-11 11:27:54 -0500

    v3.7.4 Minor change to debug tracing

--- smartapps/ScheduleTstatZones.groovy
+++ smartapps/ScheduleTstatZones.groovy
@@ -44,7 +44,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ScheduleTstatZones, the smartapp that enables Heating/Cooling zoned settings at selected thermostat(s) coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 3.7.3" 
+			paragraph "Version 3.7.4" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -1308,7 +1308,7 @@ private def adjust_thermostat_setpoint_in_zone(indiceSchedule) {
 		}
 	}
 	if (setRoomThermostatsOnly=='true') {
-		log.debug("adjust_thermostat_setpoint_in_zone>schedule ${scheduleName}: schedule ${scheduleName},all room Tstats set and setRoomThermostatsOnlyFlag= true,exiting")
+		log.debug("adjust_thermostat_setpoint_in_zone>schedule ${scheduleName}:all room Tstats set and setRoomThermostatsOnlyFlag= true,exiting")
 		return				    
 	}    
 	//	Now will do an avg temp calculation based on all temp sensors to apply the desired temp settings at the main Tstat correctly
