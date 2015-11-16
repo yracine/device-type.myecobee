f7004880c3633e22dee0102ff8bb0b4ba291212c
Yves Racine <yracine66@gmail.com>
2015-11-16 11:23:41 -0500

    v4.0 removed some check in turn_off_all_other_vents() as vent temperature is checked by an event handler

--- smartapps/ScheduleTstatZones.groovy
+++ smartapps/ScheduleTstatZones.groovy
@@ -44,7 +44,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ScheduleTstatZones, the smartapp that enables Heating/Cooling zoned settings at selected thermostat(s) coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 3.9.9" 
+			paragraph "Version 4.0" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -1595,11 +1595,14 @@ private def turn_off_all_other_vents(ventSwitchesOnSet) {
 			if (ventSwitch != null) {
 				totalVents++
 				log.debug "turn_off_all_other_vents>found=${ventSwitch}"
+
+/* Y.R. Now the check is done by an event handler
 				// Prior to any processing, check temperature in each vent to avoid any HVAC damage
 				if (is_temperature_too_hot_or_too_cold(ventSwitch)) {
 					log.debug("turn_off_all_other_vents>temperature too hot or too cold in ${ventSwitch}, exiting...")
 					return                        
 				}                    
+*/                
 				foundVentSwitch = ventSwitchesOnSet.find{it == ventSwitch}
 				if (foundVentSwitch ==null) {
 					nbClosedVents++ 
