43ad0ea03e93c59300a716d4f29b3ba3a3581ddd
Yves Racine <yracine66@gmail.com>
2015-11-16 22:03:09 -0500

    v4.8.3 Minimum open level set inside a zone

--- smartapps/ecobeeSetZoneWithSchedule.groovy
+++ smartapps/ecobeeSetZoneWithSchedule.groovy
@@ -43,7 +43,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ecobeeSetZoneWithSchedule, the smartapp that enables Heating/Cooling Zoned Solutions based on your ecobee schedule(s)- coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 4.8.2" 
+			paragraph "Version 4.8.3" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -1652,9 +1652,9 @@ private def adjust_vent_settings_in_zone(indiceSchedule) {
 				switchLevel = ((temp_diff_at_sensor / avg_temp_diff.abs()) * 100).round()
 				switchLevel =( switchLevel >=0)?((switchLevel<100)? switchLevel: 100):0
 				if (mode=='heat') {
-					switchLevel=(temp_diff_at_sensor >0)? 0: 100-switchLevel
+					switchLevel=(temp_diff_at_sensor >0)? MIN_OPEN_LEVEL_SMALL: 100-switchLevel
 				} else if (mode =='cool') {
-					switchLevel=(temp_diff_at_sensor <0)? 0: switchLevel
+					switchLevel=(temp_diff_at_sensor <0)? MIN_OPEN_LEVEL_SMALL: switchLevel
 				}                
 			} 
 			if (switchLevel >=10) {	
