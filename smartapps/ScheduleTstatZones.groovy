1381beb6eea6dd7ea08fd9ca471385a70cfda336
Yves Racine <yracine66@gmail.com>
2015-11-19 07:22:23 -0500

    v5.1 Made some changes to vents set level calculations to avoid any divided by zero exceptions

--- smartapps/ScheduleTstatZones.groovy
+++ smartapps/ScheduleTstatZones.groovy
@@ -44,7 +44,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ScheduleTstatZones, the smartapp that enables Heating/Cooling zoned settings at selected thermostat(s) coupled with smart vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 5.0.1" 
+			paragraph "Version 5.1" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -1534,12 +1534,16 @@ private def adjust_vent_settings_in_zone(indiceSchedule) {
 				}
 				float temp_diff_at_sensor = tempAtSensor.toFloat().round(1) - desiredTemp 
 				log.debug("adjust_vent_settings_in_zone>thermostat mode = ${mode}, schedule ${scheduleName}, in zone ${zoneName}, room ${roomName}, temp_diff_at_sensor=${temp_diff_at_sensor}, avg_temp_diff=${avg_temp_diff}")
-				switchLevel = ((temp_diff_at_sensor / avg_temp_diff.abs()) * 100).round()
-				switchLevel =( switchLevel >=0)?((switchLevel<100)? switchLevel: 100):0
 				if (mode=='heat') {
-					switchLevel=(temp_diff_at_sensor >=0)? MIN_OPEN_LEVEL_SMALL: 100-switchLevel
+					if (avg_temp_diff ==0) avg_temp_diff=-0.1  // to avoid divided by zero exception
+					switchLevel = ((temp_diff_at_sensor / avg_temp_diff) * 100).round()
+					switchLevel =( switchLevel >=0)?((switchLevel<100)? switchLevel: 100):0
+					switchLevel=(temp_diff_at_sensor >=0)? MIN_OPEN_LEVEL_SMALL: ((temp_diff_at_sensor <0) && (avg_temp_diff>0))?100:switchLevel
 				} else if (mode =='cool') {
-					switchLevel=(temp_diff_at_sensor <=0)? MIN_OPEN_LEVEL_SMALL: switchLevel
+					if (avg_temp_diff ==0) avg_temp_diff=0.1 // to avoid divided by zero exception
+					switchLevel = ((temp_diff_at_sensor / avg_temp_diff) * 100).round()
+					switchLevel =( switchLevel >=0)?((switchLevel<100)? switchLevel: 100):0
+					switchLevel=(temp_diff_at_sensor <=0)? MIN_OPEN_LEVEL_SMALL: ((temp_diff_at_sensor >0) && (avg_temp_diff<0))?100:switchLevel
 				}                
 			} 
 			if (switchLevel >=10) {	
