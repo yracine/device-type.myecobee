b347f8ed016c92dad48a888645a329e4aa5667d9
Yves Racine <yracine66@gmail.com>
2015-12-04 19:08:21 -0500

    v1.6.5 Minor change to some input parameters' labels

--- smartapps/ScheduleRoomTempControl.groovy
+++ smartapps/ScheduleRoomTempControl.groovy
@@ -43,7 +43,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ScheduleRoomTempControl, the smartapp that enables better temp control in rooms based on Smart Vents"
-			paragraph "Version 1.6.4" 
+			paragraph "Version 1.6.5" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -68,7 +68,7 @@ def generalSetupPage() {
 				href(name: "toRoomPage", title: "Room Setup", page: "roomsSetupPage")
 				href(name: "toZonePage", title: "Zone Setup", page: "zonesSetupPage")
 				href(name: "toSchedulePage", title: "Schedule Setup", page: "schedulesSetupPage")
-				href(name: "toNotificationsPage", title: "Notifications Setup", page: "NotificationsPage")
+				href(name: "toNotificationsPage", title: "Notification & Options Setup", page: "NotificationsPage")
 			}                
 		}
 		section("Do not set the thermostat setpoints in schedules [optional, default=The thermostat setpoints are set]") {
