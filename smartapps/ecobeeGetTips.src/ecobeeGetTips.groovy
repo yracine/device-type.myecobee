4371e58fb90f2a5d9f58f28289bcb30e772d1b52
Yves Racine <yracine66@gmail.com>
2016-09-11 10:04:16 -0400

    v1.0 added header info:  N.B. Only works with MyEcobee device v5.0 and higher.

--- smartapps/ecobeeGetTips.src/ecobeeGetTips.groovy
+++ smartapps/ecobeeGetTips.src/ecobeeGetTips.groovy
@@ -13,6 +13,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
  *
  *  Software Distribution is restricted and shall be done only with Developer's written approval.
+ *  N.B. Only works with MyEcobee device v5.0 and higher.
  */
 definition(
     name: "ecobeeGetTips",
