e109d544d8f6fe0c74829d64849fe06de834a0cc
Yves Racine <yracine66@gmail.com>
2016-09-14 09:15:19 -0400

    v1.3 Modified header

--- smartapps/ecobeeGenerateMonthlyStats.src/ecobeeGenerateMonthlyStats.groovy
+++ smartapps/ecobeeGenerateMonthlyStats.src/ecobeeGenerateMonthlyStats.groovy
@@ -32,7 +32,7 @@ definition(
 preferences {
 	section("About") {
 		paragraph "${get_APP_NAME()}, the smartapp that generates monthly runtime reports about your ecobee components"
-		paragraph "Version 1.2" 
+		paragraph "Version 1.3" 
 		paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 			href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 				title:"Paypal donation..."
