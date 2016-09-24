8f54ec4ff081b0ece36f27c512b446a71d6a684b
Yves Racine <yracine66@gmail.com>
2016-09-23 21:56:29 -0400

    v1.6.8 Corrected state.poll to atomicState.poll

--- smartapps/ecobeeGenerateMonthlyStats.src/ecobeeGenerateMonthlyStats.groovy
+++ smartapps/ecobeeGenerateMonthlyStats.src/ecobeeGenerateMonthlyStats.groovy
@@ -32,7 +32,7 @@ definition(
 preferences {
 	section("About") {
 		paragraph "${get_APP_NAME()}, the smartapp that generates monthly runtime reports about your ecobee components"
-		paragraph "Version 1.6.7"
+		paragraph "Version 1.6.8"
 		paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 			href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 				title:"Paypal donation..."
@@ -115,7 +115,7 @@ def rescheduleIfNeeded(evt) {
     
 	// Update rescheduled state
     
-	if (!evt) state.poll["rescheduled"] = now()
+	if (!evt) atomicState?.poll["rescheduled"] = now()
 }
    
 
