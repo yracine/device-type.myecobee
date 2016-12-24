dc3177cb1532cd107bfba6f76a879d283f6a58f3
Yves Racine <yracine66@gmail.com>
2016-12-23 19:00:22 -0500

    corrected donation link

--- smartapps/ecobeeControlPlug.src/ecobeeControlPlug.groovy
+++ smartapps/ecobeeControlPlug.src/ecobeeControlPlug.groovy
@@ -33,9 +33,9 @@ definition(
 preferences {
 	section("About") {
 		paragraph "ecobeeControlPlug, the smartapp that control your ecobee connected sensor or plug"
-		paragraph "Version 1.1.3" 
+		paragraph "Version 1.1.4" 
 		paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
-			href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
+			href url: "https://www.paypal.me/ecomatiqhomes",
 				title:"Paypal donation..."
 		paragraph "Copyright©2014 Yves Racine"
 			href url:"http://github.com/yracine/device-type.myecobee", style:"embedded", required:false, title:"More information..."  
