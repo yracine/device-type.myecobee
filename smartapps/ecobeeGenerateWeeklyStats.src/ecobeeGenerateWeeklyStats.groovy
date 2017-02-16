0f2b498cf92f4311a19f2fde7f22910c22937cae
Yves Racine <yracine66@gmail.com>
2017-02-16 10:54:02 -0500

    v1.6.9 Final change on notification

--- smartapps/ecobeeGenerateWeeklyStats.src/ecobeeGenerateWeeklyStats.groovy
+++ smartapps/ecobeeGenerateWeeklyStats.src/ecobeeGenerateWeeklyStats.groovy
@@ -32,7 +32,7 @@ definition(
 preferences {
 	section("About") {
 		paragraph "${get_APP_NAME()}, the smartapp that generates weekly runtime reports about your ecobee components"
-		paragraph "Version 1.6.8" 
+		paragraph "Version 1.6.9" 
 		paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 			href url: "https://www.paypal.me/ecomatiqhomes",
 				title:"Paypal donation..."
@@ -54,7 +54,7 @@ preferences {
 	section( "Notifications" ) {
 		input "sendPushMessage", "enum", title: "Send a push notification?", metadata:[values:["Yes", "No"]], required: false, default:"No"
 		input "phoneNumber", "phone", title: "Send a text message?", required: false
-    }
+	}
 	section("Detailed Notifications") {
 		input "detailedNotif", "bool", title: "Detailed Notifications?", required:false
     }
@@ -373,7 +373,7 @@ void generateRuntimeReport(component, startDate, endDate, frequence='daily') {
 private send(msg, askAlexa=false) {
 	def message = "${get_APP_NAME()}>${msg}"
 	def sendNotification= (sendPushMessage==null) ?: "No"
-	if (sendNotification != "No") {
+	if (sendNotification == "Yes") {
 		
 		if (askAlexa) {
 			sendLocationEvent(name: "AskAlexaMsgQueue", value: "${get_APP_NAME()}", isStateChange: true, descriptionText: msg)        
