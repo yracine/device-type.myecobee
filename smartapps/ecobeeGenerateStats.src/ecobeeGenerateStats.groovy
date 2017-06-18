44348bba872ead443c5db66a62392f92885c0ec8
Yves Racine <yracine66@gmail.com>
2017-06-18 17:22:35 -0400

    v2.5 Added log.info for Queue refresh in AskAlexaMQHandler

--- smartapps/ecobeeGenerateStats.src/ecobeeGenerateStats.groovy
+++ smartapps/ecobeeGenerateStats.src/ecobeeGenerateStats.groovy
@@ -112,6 +112,7 @@ def askAlexaMQHandler(evt) {
 	switch (evt.value) {
 		case "refresh":
 		state?.askAlexaMQ = evt.jsonData && evt.jsonData?.queues ? evt.jsonData.queues : []
+		log.info("askAlexaMQHandler>refresh value=$state?.askAlexaMQ")        
 		break
 	}
 }
