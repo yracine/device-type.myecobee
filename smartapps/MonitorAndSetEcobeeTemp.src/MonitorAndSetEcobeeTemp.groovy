08d40d514b65f7021009132b8ded385e7aa422bb
Yves Racine <yracine66@gmail.com>
2017-09-17 21:38:16 -0400

    v3.4.9 minor change to max_temp_diff

--- smartapps/MonitorAndSetEcobeeTemp.src/MonitorAndSetEcobeeTemp.groovy
+++ smartapps/MonitorAndSetEcobeeTemp.src/MonitorAndSetEcobeeTemp.groovy
@@ -746,7 +746,7 @@ private def check_if_hold_needed() {
 		if (state.tempSensors) {
 			temp_diff = (ecobeeTemp - avg_indoor_temp).round(1) // adjust the coolingSetPoint at the ecobee tstat according to the avg indoor temp measured
                 
-			temp_diff = (temp_diff <0-max_temp_diff)?max_temp_diff:(temp_diff >max_temp_diff)?max_temp_diff:temp_diff // determine the temp_diff based on max_temp_diff
+			temp_diff = (temp_diff <0-max_temp_diff)? (-max_temp_diff):(temp_diff >max_temp_diff)?max_temp_diff:temp_diff // determine the temp_diff based on max_temp_diff
 			targetTstatTemp = (programCoolTemp - temp_diff).round(1)
 			if (temp_diff.abs() > MIN_TEMP_DIFF) {  // adust the temp only if temp diff is significant
 				ecobee.setCoolingSetpoint(targetTstatTemp)
@@ -801,7 +801,7 @@ private def check_if_hold_needed() {
 		} /* if outdoorSensor */
 		if (state.tempSensors) {
 			temp_diff = (ecobeeTemp - avg_indoor_temp).round(1) // adjust the heatingSetPoint at the tstat according to the avg indoor temp measur
-			temp_diff = (temp_diff <0-max_temp_diff)?max_temp_diff:(temp_diff >max_temp_diff)?max_temp_diff:temp_diff // determine the temp_diff based on max_temp_diff
+			temp_diff = (temp_diff <0-max_temp_diff)?(-max_temp_diff):(temp_diff >max_temp_diff)?max_temp_diff:temp_diff // determine the temp_diff based on max_temp_diff
 			targetTstatTemp = (programHeatTemp + temp_diff).round(1)
 			if (temp_diff.abs() > MIN_TEMP_DIFF) {  // adust the temp only if temp diff is significant
 				ecobee.setHeatingSetpoint(targetTstatTemp)
