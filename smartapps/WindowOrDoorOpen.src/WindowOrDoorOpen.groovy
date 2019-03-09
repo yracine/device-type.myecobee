d1b4629812360156386b6c86a83f0e28f338def5
Yves Racine <yracine66@gmail.com>
2019-03-09 18:28:30 -0500

    Update WindowOrDoorOpen.groovy

--- smartapps/WindowOrDoorOpen.src/WindowOrDoorOpen.groovy
+++ smartapps/WindowOrDoorOpen.src/WindowOrDoorOpen.groovy
@@ -13,7 +13,7 @@
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
  *
- * Software Distribution is restricted and shall be done fonly with Developer's written approval.
+ * Software Distribution is restricted and shall be done only with Developer's written approval.
  * 
  * Compatible with MyEcobee & MyNextTstat devices available at my store:
  *          http://www.ecomatiqhomes.com/#!store/tc3yr 
