f667021a857f4ca0e36c09858e775b74a3b77e83
Yves Racine <yracine66@gmail.com>
2020-05-07 10:33:35 -0400

    v2.7.2 Minor changes

--- smartapps/WindowOrDoorOpen.src/WindowOrDoorOpen.groovy
+++ smartapps/WindowOrDoorOpen.src/WindowOrDoorOpen.groovy
@@ -1,7 +1,7 @@
 /**
  *  WindowOrDoorOpen
  *
- *  Copyright 2014 Yves Racine 
+ *  Copyright 2014-2020 Yves Racine 
  *  LinkedIn profile: ca.linkedin.com/pub/yves-racine-m-sc-a/0/406/4b/
  *
  *  Developer retains all right, title, copyright, and interest, including all copyright, patent rights, trade secret 
@@ -34,7 +34,7 @@ preferences {
 		paragraph "WindowOrDoorOpen!, the smartapp that warns you if you leave a door or window open (with voice as an option);" +
 			"(optional) Your thermostats can be turned off or set to eco/away after a delay and restore their mode when the contact is closed." +
     		"The smartapp can track up to 30 contacts and can keep track of 6 open contacts at the same time due to ST scheduling limitations"
-		paragraph "Version 2.7.1" 
+		paragraph "Version 2.7.2" 
 		paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 			href url: "https://www.paypal.me/ecomatiqhomes",
 					title:"Paypal donation..."            
@@ -76,6 +76,10 @@ preferences {
 def installed() {
 	log.debug "Installed with settings: ${settings}"
 
+	state?.status=[]    
+	state?.count=[]    
+	state?.lastThermostatMode = null
+	state.lastThermostatMode = ""
 	initialize()
 }
 
@@ -83,17 +87,13 @@ def updated() {
 	log.debug "Updated with settings: ${settings}"
 
 	unsubscribe()
-	unschedule()    
+//	unschedule()    
 	initialize()
 }
 
 def initialize() {	
 	def MAX_CONTACT=30
-	state?.lastThermostatMode = null
 	// subscribe to all contact sensors to check for open/close events
-	state?.status=[]    
-	state?.count=[]    
-	state.lastThermostatMode = ""
 	if (holdSwitch) {
 		subscribe(holdSwitch, "switch.off", offHandler)
 		subscribe(holdSwitch, "switch.on", onHandler)
@@ -665,6 +665,7 @@ private void restore_tstats_mode() {
 		if (lastSavedMode) {
 			log.debug "restore_tstats_mode>about to set ${it}, back to saved thermostatMode=${lastSavedMode}"
 			String currentMode= it.currentThermostatMode
+			if ((currentMode== 'eco') && (it.hasCommand("home"))) it.home()        
 			if (lastSavedMode == 'cool') {
 				it.cool()
 			} else if (lastSavedMode.contains('heat')) {
